17c25372f81c6abcb012bb325c22c00150d83fea
Jonatan Vahlberg <jonatan.vahlberg@willandskill.se>
2024-03-01 14:28:19 +0100

    add moment task solution

--- moment.js
+++ moment.js
@@ -1,30 +1,61 @@
+const moment = require("moment");
+require("moment-timezone");
 
-const moment = require("moment")
-require("moment-timezone")
+function momentExp() {
+  // Create moment instance
+  const now = moment();
 
-// Create moment instance
-const now = moment();
+  // Format output into string
+  console.log(now.format("YYYY-MM-DD HH:mm:ss"));
+  console.log(now.format("YYYY MMM dddd DD"));
 
-// Format output into string
-console.log(now.format("YYYY-MM-DD HH:mm:ss"))
-console.log(now.format("YYYY MMM dddd DD"))
+  // Change time
+  const saturday = now.clone().add(1, "days").set("hour", 0);
 
-// Change time
-const saturday = now.clone().add(1, "days").set("hour", 0)
+  console.log(saturday.format("YYYY-MM-DD HH:mm:ss"));
 
-console.log(saturday.format("YYYY-MM-DD HH:mm:ss"))
+  // Comparisons
+  console.log(now);
+  const todayIsBeforeSaturday = now.isBefore(saturday);
+  console.log("Now is before saturday", todayIsBeforeSaturday);
 
-// Comparisons
-console.log(now)
-const todayIsBeforeSaturday = now.isBefore(saturday)
-console.log("Now is before saturday", todayIsBeforeSaturday)
+  // Convert timezone
+  const timezone = moment().tz("Europe/Stockholm");
+
+  const timeInAsia = timezone.clone().tz("Asia/Seoul");
+
+  console.log(timezone.format("HH:mm z"), timeInAsia.format("HH:mm z"));
+
+  console.log(timezone.utc().format("HH:mm"));
+}
+
+// momentExp()
+
+function checkBooking() {
+    const bookedDatetimes = [
+      "2023-01-23T08:00",
+      "2023-02-15T12:30",
+      "2023-02-15T13:30",
+      "2023-03-10T09:45",
+    ];
+
+    function isDatetimeBooked(bookedDatetimes = [], datetime) {
+        let isBooked = false
+
+        isBooked = bookedDatetimes.some((dt) => {
+            const dtMoment = moment(dt);
+            const datetimeMoment = moment(datetime);
+            const isSame = dtMoment.isSame(datetimeMoment)
+            const diff = Math.abs(dtMoment.diff(datetimeMoment, "hour"));
+            return isSame || diff === 0
+        })
+
+        return isBooked
+    }
+    
+    console.log("Datetime is booked ", isDatetimeBooked(bookedDatetimes, "2023-02-15T14:31"));
 
 
-// Convert timezone
-const timezone = moment().tz("Europe/Stockholm")
+}
 
-const timeInAsia = timezone.clone().tz("Asia/Seoul")
-
-console.log(timezone.format("HH:mm z"), timeInAsia.format("HH:mm z"))
-
-console.log(timezone.utc().format("HH:mm"))
+checkBooking();
