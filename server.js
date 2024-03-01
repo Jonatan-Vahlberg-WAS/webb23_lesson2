29eed184c8b3a93324d4cbfd92d4c1fa6f888781
Jonatan Vahlberg <jonatan.vahlberg@willandskill.se>
2024-03-01 10:40:21 +0100

    finished basic server

--- server.js
+++ server.js
@@ -1,23 +1,21 @@
-const http = require('http');
-
+const http = require("http");
 
 const server = http.createServer((req, res) => {
-    console.log(req.url)
-    if (req.url === '/') {
-        res.writeHead(
-            200,
-            { 'Content-Type': "text/plain" }
-        )
-        return res.end("Welcome to our webbserver!")
-    }
+  console.log(req.url);
+  if (req.url === "/") {
+    res.writeHead(200, { "Content-Type": "text/plain" });
+    return res.end("Welcome to our webbserver!");
+  }
+    if (req.url === "/contact") {
+        res.writeHead(200, { "Content-Type": "text/plain" });
+        res.end(`Contact us on\nName: Jonatan v\nPhone: 071234567\nEmail: email@domain.com`);
+  }
 
-    res.writeHead(404, { "Content-Type": "text/plain" });
-    res.end("404: page is not a part of our webbserver")
-    
-    
-})
-const port = 3000
+  res.writeHead(404, { "Content-Type": "text/plain" });
+  res.end("404: page is not a part of our webbserver");
+});
+
+const port = 3000;
 server.listen(port, () => {
-    console.log("Server is running on port ", port)
-})
-
+  console.log("Server is running on port ", port);
+});
