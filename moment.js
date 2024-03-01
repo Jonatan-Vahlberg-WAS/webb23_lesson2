8570bc8a5d7b4a1657c8de48d0777309f971a230
Jonatan Vahlberg <jonatan.vahlberg@willandskill.se>
2024-03-01 14:00:00 +0100

    push basic moment config

---
+++ moment.js
@@ -0,0 +1,35 @@
+
+const moment = require("moment")
+require("moment-timezone")
+
+// Create moment instance
+const now = moment();
+
+// Format output into string
+console.log(now.format("YYYY-MM-DD HH:mm:ss"))
+console.log(now.format("YYYY MMM dddd DD"))
+
+// Change time
+const saturday = now.clone().add(1, "days").set("hour", 0)
+
+console.log(saturday.format("YYYY-MM-DD HH:mm:ss"))
+
+// Comparisons
+console.log(now)
+const todayIsBeforeSaturday = now.isBefore(saturday)
+console.log("Now is before saturday", todayIsBeforeSaturday)
+
+
+// Convert timezone
+const timezone = moment().tz("Europe/Stockholm")
+
+const timeInAsia = timezone.clone().tz("Asia/Seoul")
+
+console.log(timezone.format("HH:mm z"), timeInAsia.format("HH:mm z"))
+
+console.log(timezone.utc().format("HH:mm"))
+
+
+// moment.locale("sv")
+
+console.log(moment().format("YYYY MMMM DD"))
